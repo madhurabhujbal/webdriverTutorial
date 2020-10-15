0121364cc9ae667609d4bc79ad45113791539c6f
Madhura <madhura.bhujbal@gmail.com>
2020-10-15 14:54:44 +0100

    Change findElements method to findElement

--- library.js
+++ library.js
@@ -5,8 +5,8 @@ var webdriver = require("selenium-webdriver"),
 async function findElementOnWebsite() {
   var driver = new webdriver.Builder().forBrowser("chrome").build();
   await driver.get("https://library-app.firebaseapp.com");
-  await driver.findElements(By.css("input"));
-  await driver.findElements(By.css(".btn-primary"));
+  await driver.findElement(By.css("input"));
+  await driver.findElement(By.css(".btn-primary"));
 
   (await driver).sleep(1000);
   driver.quit();
