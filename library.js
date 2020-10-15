fe8516e6acb4f80d95b555f47742d87eee431c38
Madhura <madhura.bhujbal@gmail.com>
2020-10-15 16:59:06 +0100

    Get list of navbar items

--- library.js
+++ library.js
@@ -2,22 +2,26 @@ var webdriver = require("selenium-webdriver"),
   By = webdriver.By,
   until = webdriver.until;
 
-async function findElementOnWebsite() {
-  var driver = new webdriver.Builder().forBrowser("chrome").build();
-  await driver.get("https://library-app.firebaseapp.com");
-  await driver.findElement(By.css("input")).then((el) => {
+var driver = new webdriver.Builder().forBrowser("chrome").build();
+driver.get("https://library-app.firebaseapp.com");
+driver.findElement(By.css("input")).then((el) => {
     console.log("found input box", el);
-  });
-  await driver
-    .findElement(By.css(".btn-primary"))
-    .getText()
-    .then((el) => {
-      console.log("found button element with text : ", el);
+});
+
+driver
+  .findElement(By.css(".btn-primary"))
+  .getText()
+  .then((el) => {
+  console.log("found button element with text : ", el);
+});
+
+   driver.findElements(By.css("nav li")).then((elements) => {
+      elements.map((el) => {
+        el.getText().then((text) => {
+          console.log("Navbar element text : " + text);
+        });
+      });
     });
-  await driver.findElements(By.css("nav li"));
 
-  (await driver).sleep(1000);
-  driver.quit();
-}
-
-findElementOnWebsite();
+// driver.sleep(1000);
+// driver.quit();
