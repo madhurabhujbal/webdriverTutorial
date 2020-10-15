c33a126e265531e1158e8e7e3947a5cb4c155f0e
Madhura <madhura.bhujbal@gmail.com>
2020-10-15 13:46:26 +0100

    Write test code for opening library App in driver automatically

--- library.js
+++ library.js
@@ -4,4 +4,4 @@ var webdriver = require("selenium-webdriver"),
 
 var driver = new webdriver.Builder().forBrowser("chrome").build();
 
-driver.get("http://www.google.com");
+driver.get("https://library-app.firebaseapp.com");
