e96fe32371eea270855bf2ebdb66461741e1b7c9
Madhura <madhura.bhujbal@gmail.com>
2020-10-16 10:58:55 +0100

    Add code to enter data in input field and click button on entering data

--- library.js
+++ library.js
@@ -5,23 +5,28 @@ until = webdriver.until;
 async function findElementOnWebsite() {
   var driver = new webdriver.Builder().forBrowser("chrome").build();
   await driver.get("https://library-app.firebaseapp.com");
-  await driver.findElement(By.css("input")).then((el) => {
-    console.log("found input box", el);
-  });
-  await driver
-    .findElement(By.css(".btn-primary"))
-    .getText()
-    .then((el) => {
-      console.log("found button element with text : ", el);
-    });
 
-  let elements = await driver.findElements(By.css("nav li"));
-  for( let e of elements) {
-    let text = await e.getText();
-    console.log("Navbar element text : " + text);
-  }
+  (await driver).findElement(By.css('input')).sendKeys('username@user.com');
+  (await driver).sleep(10000);
+  (await driver).findElement(By.css(".btn-primary")).click();
+
+  // await driver.findElement(By.css("input")).then((el) => {
+  //   console.log("found input box", el);
+  // });
+  // await driver
+  //   .findElement(By.css(".btn-primary"))
+  //   .getText()
+  //   .then((el) => {
+  //     console.log("found button element with text : ", el);
+  //   });
+
+  // let elements = await driver.findElements(By.css("nav li"));
+  // for( let e of elements) {
+  //   let text = await e.getText();
+  //   console.log("Navbar element text : " + text);
+  // }
   // (await driver).sleep(1000);
-  driver.close();
+  //driver.close();
 }
 
 findElementOnWebsite();
