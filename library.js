8f31e00893deb189b7399403b219875e1dff367f
Madhura <madhura.bhujbal@gmail.com>
2020-10-18 12:09:29 +0100

    Add implicit wait command for instructing browser to wait until it finds and element on page

--- library.js
+++ library.js
@@ -5,10 +5,11 @@ until = webdriver.until;
 async function findElementOnWebsite() {
   var driver = new webdriver.Builder().forBrowser("chrome").build();
   await driver.get("https://library-app.firebaseapp.com");
+  (await driver).manage().setTimeouts({ implicit:5000 });
 
    let inputElement = (await driver).findElement(By.css('input'));
    await inputElement.sendKeys('username@user.com');
-    (await driver).sleep(1000);
+    (await driver).sleep(5000);
 
    let requestButton = (await driver).findElement(By.css(".btn-primary"));
    await requestButton.click();
