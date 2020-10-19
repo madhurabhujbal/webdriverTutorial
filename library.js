03933d7ffb088943d31295fea8ee6480f4364785
Madhura <madhura.bhujbal@gmail.com>
2020-10-19 17:39:47 +0100

    Add code for driver to explicitly wait until element is located

--- library.js
+++ library.js
@@ -5,13 +5,15 @@ until = webdriver.until;
 async function findElementOnWebsite() {
   var driver = new webdriver.Builder().forBrowser("chrome").build();
   await driver.get("https://library-app.firebaseapp.com");
-  (await driver).manage().setTimeouts({ implicit:5000 });
+  // implicit wait : (await driver).manage().setTimeouts({ implicit:5000 });
 
    let inputElement = (await driver).findElement(By.css('input'));
    await inputElement.sendKeys('username@user.com');
 
    let requestButton = (await driver).findElement(By.css(".btn-primary"));
    await requestButton.click();
+
+   await driver.wait(until.elementLocated(By.css('.alert-success')),4000);
 
    let alertTextPromise =  (await driver).findElement(By.css(".alert-success"));
    let alertText = await alertTextPromise.getText();
