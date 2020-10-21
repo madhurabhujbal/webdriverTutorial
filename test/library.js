80ed3f35723780183f73cbffc9006bc2c2c5d69b
Madhura <madhura.bhujbal@gmail.com>
2020-10-21 14:56:27 +0100

    Remove unwanted code

--- test/library.js
+++ test/library.js
@@ -22,12 +22,8 @@ describe("library app scenarios", function() {
    await inputElement.sendKeys('user@username.com');
 
    let requestButton = (await driver).findElement(By.css(".btn-primary"));
-    // (await driver.wait(async () => {
        const result = await requestButton.getCssValue('opacity');
         assert(result === '1');
-      // return result == 1;
-    //  }
-    //  , 5000));
   });
 
   it('Gives alert message on button clicked', async function() {
