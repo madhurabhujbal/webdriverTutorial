a6601c14bba8a5575024a4a8936407cf8dd2adca
Madhura <madhura.bhujbal@gmail.com>
2020-10-21 12:51:19 +0100

    Create test using mocha

---
+++ test/library.js
@@ -0,0 +1,69 @@
+var webdriver = require("selenium-webdriver")
+var { describe, it, after, before } = require("mocha");
+By = webdriver.By,
+until = webdriver.until;
+var driver;
+
+describe("library app scenarios", function() {
+  this.timeout(7000);
+  beforeEach(async function() {
+    driver = new webdriver.Builder().forBrowser("chrome").build();
+   await driver.get("https://library-app.firebaseapp.com");
+
+  });
+
+  afterEach(function() {
+    driver.close();
+  });
+
+  it('Changes the button opacity on valid email in input field', async function() {
+    let inputElement = (await driver).findElement(By.css('input'));
+   await inputElement.sendKeys('us@h.com');
+
+   let requestButton = (await driver).findElement(By.css(".btn-primary"));
+    (await driver.wait(async () => {
+       const result = await requestButton.getCssValue('opacity');
+      return result == 1;
+     }, 15000));
+  });
+
+  it('doesnt work with mocha', async function() {
+    let inputElement = (await driver).findElement(By.css('input'));
+   await inputElement.sendKeys('us.com');
+
+   let requestButton = (await driver).findElement(By.css(".btn-primary"));
+    await requestButton.click();
+   let alertText = (await (await driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
+   console.log("alert text is : ", alertText);
+  });
+
+  it('works with mocha', async function() {
+    await driver.findElement(By.css('nav')).getText().then((text) => {
+      console.log(text);
+    });
+  });
+
+});
+
+// async function findElementOnWebsite() {
+  // var driver = new webdriver.Builder().forBrowser("chrome").build();
+  // await driver.get("https://library-app.firebaseapp.com");
+  // implicit wait : (await driver).manage().setTimeouts({ implicit:5000 });
+
+  //  let inputElement = (await driver).findElement(By.css('input'));
+  //  await inputElement.sendKeys('us@h.com');
+
+  //  let requestButton = (await driver).findElement(By.css(".btn-primary"));
+  //   (await driver.wait(async () => {
+  //      const result = await requestButton.getCssValue('opacity');
+  //     return result == 1;
+  //    }, 15000));
+  //  await requestButton.click();
+
+  //  let alertText = (await (await driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
+  //  console.log("alert text is : ", alertText);
+
+  // driver.close();
+// }
+
+// findElementOnWebsite();
