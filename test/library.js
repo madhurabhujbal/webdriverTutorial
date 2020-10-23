200102fdc4d3cbac68c5d41e0e49acfc8c7a5a69
Madhura <madhura.bhujbal@gmail.com>
2020-10-23 11:10:10 +0100

    Import By and until and run tests by replacing driver with page.dirver

--- test/library.js
+++ test/library.js
@@ -1,5 +1,7 @@
 var webdriver = require("selenium-webdriver"),
   { describe, it, after, before } = require("mocha");
+  By = webdriver.By,
+until = webdriver.until;
 var Page = require('../lib/base_page');
 var page;
 
@@ -17,31 +19,31 @@ describe("library app scenarios", function() {
   });
 
   it('Changes the button opacity on valid email in input field', async function() {
-  //   let inputElement = (await driver).findElement(By.css('input'));
-  //  await inputElement.sendKeys('user@username.com');
+    let inputElement = (await page.driver).findElement(By.css('input'));
+   await inputElement.sendKeys('user@username.com');
 
-  //  let requestButton = (await driver).findElement(By.css(".btn-primary"));
-  //  (await driver.wait(async () => {
-  //     const result = await requestButton.getCssValue('opacity');
-  //    return result == 1;
-  //   }, 15000));
+   let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
+   (await page.driver.wait(async () => {
+      const result = await requestButton.getCssValue('opacity');
+     return result == 1;
+    }, 15000));
   console.log("hello");
   });
 
   it('Gives alert message on button clicked', async function() {
-  //   let inputElement = (await driver).findElement(By.css('input'));
-  //  await inputElement.sendKeys('user@username.com');
+    let inputElement = (await page.driver).findElement(By.css('input'));
+   await inputElement.sendKeys('user@username.com');
 
-  //  let requestButton = (await driver).findElement(By.css(".btn-primary"));
-  //   await requestButton.click();
-  //  let alertText = (await (await driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
-  //  console.log("alert text is : ", alertText);
+   let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
+    await requestButton.click();
+   let alertText = (await (await page.driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
+   console.log("alert text is : ", alertText);
   });
 
-  // it('Display navbar elements', async function() {
-  //   await driver.findElements(By.css('nav')).then((result) => {
-  //    console.log(result);
-  //   });
-  // });
+  it('Display navbar elements', async function() {
+    await page.driver.findElements(By.css('nav')).then((result) => {
+     console.log(result);
+    });
+  });
   console.log("hello");
 });
