27202946d30017d33cbbd8c43e7d781f24ae36b8
Madhura <madhura.bhujbal@gmail.com>
2020-10-23 16:14:28 +0100

    Move the test code to home_page

--- test/library.js
+++ test/library.js
@@ -2,11 +2,11 @@ var webdriver = require("selenium-webdriver"),
   { describe, it, after, before } = require("mocha");
 //   By = webdriver.By,
 // until = webdriver.until;
-var Page = require('../lib/base_page');
+var Page = require('../lib/home_page');
 var page;
 
 describe("library app scenarios", function() {
-  this.timeout(7000);
+  this.timeout(9000);
 
   beforeEach(async function() {
     page = new Page();
@@ -15,35 +15,38 @@ describe("library app scenarios", function() {
   });
 
   afterEach(function() {
-    page.driver.close();
+    page.close();
   });
 
-  it('Changes the button opacity on valid email in input field', async function() {
-    let inputElement = (await page.driver).findElement(By.css('input'));
-   await inputElement.sendKeys('user@username.com');
+  it('Typing valid email changes button opacity to 1', async function() {
+    await page.requestButton();
+  //   let inputElement = (await page.driver).findElement(By.css('input'));
+  //  await inputElement.sendKeys('user@username.com');
 
-   let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
-   (await page.driver.wait(async () => {
-      const result = await requestButton.getCssValue('opacity');
-     return result == 1;
-    }, 15000));
-  console.log("hello");
+  //  let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
+  //  (await page.driver.wait(async () => {
+  //     const result = await requestButton.getCssValue('opacity');
+  //    return result == 1;
+  //   }, 15000));
+  // console.log("hello");
   });
 
-  it('Gives alert message on button clicked', async function() {
-    let inputElement = (await page.driver).findElement(By.css('input'));
-   await inputElement.sendKeys('user@username.com');
+  it('Typing a valid email enables request button', async function() {
+    await page.requestButton();
+  //   let inputElement = (await page.driver).findElement(By.css('input'));
+  //  await inputElement.sendKeys('user@username.com');
 
-   let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
-    await requestButton.click();
-   let alertText = (await (await page.driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
-   console.log("alert text is : ", alertText);
+  //  let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
+  //   await requestButton.click();
+  //  let alertText = (await (await page.driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
+  //  console.log("alert text is : ", alertText);
   });
 
-  it('Display navbar elements', async function() {
-    await page.driver.findElements(By.css('nav')).then((result) => {
-     console.log(result);
-    });
-  });
-  console.log("hello");
+  it('Clicking Request invitation triggers confirmation box', async function() {
+    await page.alertSuccess();
+  //   await page.driver.findElements(By.css('nav')).then((result) => {
+  //    console.log(result);
+  //   });
+   });
+  // console.log("hello");
 });
