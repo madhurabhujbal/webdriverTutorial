24af105f3219c50f85342e28717236d1fe43ba30
Madhura <madhura.bhujbal@gmail.com>
2020-10-24 10:24:18 +0100

    Delete unwanted code

--- test/library.js
+++ test/library.js
@@ -1,7 +1,5 @@
 var webdriver = require("selenium-webdriver"),
   { describe, it, after, before } = require("mocha");
-//   By = webdriver.By,
-// until = webdriver.until;
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var should = chai.should();
@@ -15,7 +13,6 @@ describe("library app scenarios", function() {
   beforeEach(async function() {
     page = new Page();
    await page.visit("https://library-app.firebaseapp.com");
-
   });
 
   afterEach(function() {
@@ -25,35 +22,15 @@ describe("library app scenarios", function() {
   it('Typing valid email changes button opacity to 1', async function() {
     var button = await page.requestButton();
     button.opacity.should.eventually.equal('1');
-  //   let inputElement = (await page.driver).findElement(By.css('input'));
-  //  await inputElement.sendKeys('user@username.com');
-
-  //  let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
-  //  (await page.driver.wait(async () => {
-  //     const result = await requestButton.getCssValue('opacity');
-  //    return result == 1;
-  //   }, 15000));
-  // console.log("hello");
   });
 
   it('Typing a valid email enables request button', async function() {
     var enableButton = await page.requestButton();
     await enableButton.state.should.eventually.be.true;
-  //   let inputElement = (await page.driver).findElement(By.css('input'));
-  //  await inputElement.sendKeys('user@username.com');
-
-  //  let requestButton = (await page.driver).findElement(By.css(".btn-primary"));
-  //   await requestButton.click();
-  //  let alertText = (await (await page.driver.wait(until.elementLocated(By.css('.alert-success')),4000)).getText());
-  //  console.log("alert text is : ", alertText);
   });
 
   it('Clicking Request invitation triggers confirmation box', async function() {
     var alertMsg = page.alertSuccess();
     await alertMsg.should.eventually.contain("Thank you!");
-  //   await page.driver.findElements(By.css('nav')).then((result) => {
-  //    console.log(result);
-  //   });
    });
-  // console.log("hello");
 });
